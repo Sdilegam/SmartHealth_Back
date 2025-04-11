e0c2ad6107450bc6c247ddcc9e68f2146735455e
Salvatore <83271357+Sdilegam@users.noreply.github.com>
2025-04-11 15:45:19 +0200

    Adding avatar to Doctors

---
+++ SmartHealth_Infrastructure/Migrations/20250411113625_Avatar.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHealth_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Avatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Doctors");
+        }
+    }
+}
